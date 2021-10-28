00e6120fb1fd6361025493ef3021e1e7bbce1036
PandA <1554296563@qq.com>
2021-10-28 20:40:41 +0800

    error

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -14,18 +14,18 @@ module.exports = {
     ],
     module: {
         rules: [{
-            test: /\.s[ac]ss$/i,
+            test: /\.scss$/i,
             use: [
                 "style-loader",
                 "css-loader",
                 {
                     loader: 'sass-loader',
                     options: {
-                        implementation: require('dart-sass'),
-                    },
-                },
-            ],
-        }, ],
-    },
+                        implementation: require('dart-sass')
+                    }
+                }
+            ]
+        }]
+    }
 
 }
