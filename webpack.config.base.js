ce5bf031a688588c57e7d06a84d23b7529abcd96
PandA <1554296563@qq.com>
2021-10-29 11:43:58 +0800

    准备提交了

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -14,18 +14,31 @@ module.exports = {
     ],
     module: {
         rules: [{
-            test: /\.scss$/i,
-            use: [
-                "style-loader",
-                "css-loader",
-                {
-                    loader: 'sass-loader',
-                    options: {
-                        implementation: require('dart-sass')
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [{
+                    loader: 'file-loader',
+                }, ],
+            }, {
+                test: /\.styl$/,
+                loader: ["style-loader", "css-loader", "stylus-loader", ]
+            }, {
+                test: /\.less$/i,
+                loader: ["style-loader", "css-loader", "less-loader", ],
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('dart-sass')
+                        }
                     }
-                }
-            ]
-        }]
+                ]
+            }
+        ]
     }
 
 }
